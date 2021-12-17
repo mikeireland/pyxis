306742981b0e35140511adb6e281ecefc8c9132e
Bohlsen <79351569+Bohlsen@users.noreply.github.com>
2021-12-17 17:21:47 +1100

    Add files via upload
    Header file describing error codes which the device can report to the host

---
+++ robot/firmware/ErrorCodes.h
@@ -0,0 +1,6 @@
+//This file includes a series of macros which are reported
+//by the device back to the host as error codes.
+
+#define UnrecVal 0xFD //If the code of a command is unrecognised
+#define SchedFull 0xFE //If the schedule is full
+#define PackFail 0xFF //If a received packet does not begin correctly
