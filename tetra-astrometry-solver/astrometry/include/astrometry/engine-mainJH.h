ce6218959fc8b72e6d438a320d7cf027cc3e4c2c
Jonah <jonah.hansen@anu.edu.au>
2022-02-10 17:54:12 +1100

    New working folder for plate solver c++

---
+++ tetra-astrometry-solver/astrometry/include/astrometry/engine-mainJH.h
@@ -0,0 +1,8 @@
+#ifndef ENGINE_MAINJH_H
+#define ENGINE_MAINJH_H
+
+#include <stdio.h>
+
+int main_solver(int argc, char** args);
+
+#endif
---
+++ tetra-astrometry-solver/astrometry/solver/mymain.c
@@ -0,0 +1,8 @@
+#include "engine-mainJH.h"
+#include <stdio.h>
+
+int main(int argc, char** args) {
+    printf("I'm helpful!\n\n");
+    int x = main_solver(argc, args);
+    return x;
+}
