f605f810051675843c8b7a9b7cb605439f73d624
Joseph Mangos <u6452801@anu.edu.au>
2022-02-10 16:28:28 +1100

    minor changes

--- linux driver/AccelerationTestSource.cpp
+++ linux driver/AccelerationTestSource.cpp
@@ -17,6 +17,32 @@ int main() {
 
     bool run_test_flag = true;
 
+
+    for(int i = 0; i < 1000; i++) {
+		driver.RequestAccelerations();
+		driver.teensy_port.PacketManager();
+
+		tester.acc0_ground_state_.x += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer0_in_.x[0],driver.teensy_port.accelerometer0_in_.x[1]);
+		tester.acc0_ground_state_.y += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer0_in_.y[0],driver.teensy_port.accelerometer0_in_.y[1]);
+		tester.acc0_ground_state_.z += -0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer0_in_.z[0],driver.teensy_port.accelerometer0_in_.z[1]);
+		tester.acc1_ground_state_.x += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer1_in_.x[0],driver.teensy_port.accelerometer1_in_.x[1]);
+		tester.acc1_ground_state_.y += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer1_in_.y[0],driver.teensy_port.accelerometer1_in_.y[1]);
+		tester.acc1_ground_state_.z += -0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer1_in_.z[0],driver.teensy_port.accelerometer1_in_.z[1]);
+		tester.acc2_ground_state_.x += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer2_in_.x[0],driver.teensy_port.accelerometer2_in_.x[1]);
+		tester.acc2_ground_state_.y += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer2_in_.y[0],driver.teensy_port.accelerometer2_in_.y[1]);
+		tester.acc2_ground_state_.z += -0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer2_in_.z[0],driver.teensy_port.accelerometer2_in_.z[1]);
+		tester.acc3_ground_state_.x += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer3_in_.x[0],driver.teensy_port.accelerometer3_in_.x[1]);
+		tester.acc3_ground_state_.y += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer3_in_.y[0],driver.teensy_port.accelerometer3_in_.y[1]);
+		tester.acc3_ground_state_.z += -0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer3_in_.z[0],driver.teensy_port.accelerometer3_in_.z[1]);
+		tester.acc4_ground_state_.x += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer4_in_.x[0],driver.teensy_port.accelerometer4_in_.x[1]);
+		tester.acc4_ground_state_.y += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer4_in_.y[0],driver.teensy_port.accelerometer4_in_.y[1]);
+		tester.acc4_ground_state_.z += -0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer4_in_.z[0],driver.teensy_port.accelerometer4_in_.z[1]);
+		tester.acc5_ground_state_.x += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer5_in_.x[0],driver.teensy_port.accelerometer5_in_.x[1]);
+		tester.acc5_ground_state_.y += 0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer5_in_.y[0],driver.teensy_port.accelerometer5_in_.y[1]);
+		tester.acc5_ground_state_.z += -0.001*AccelerationBytesToPhysicalDouble(driver.teensy_port.accelerometer5_in_.z[0],driver.teensy_port.accelerometer5_in_.z[1]);
+		usleep(500);
+	}
+
     //We run repeatedly run the acceleration wave tests until we cancel out of them
     while(run_test_flag) {
         time_current = high_resolution_clock::now();
