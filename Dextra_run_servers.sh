5c39363adc7191739d325f7d2701a0a33a664d64
JonahHansen <hansen.jonah833@gmail.com>
2023-05-24 23:40:20 +1000

    screen script

---
+++ Dextra_run_servers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Setup cron jobs for cleaning out the camera and platesolver directories
+(crontab -l 2>/dev/null; echo "*/2 * * * * (cd ~/GitRepos/pyxis/servers/coarse_star_tracker/data && ls -tp | grep -v '/$' | tail -n +11 | xargs -d '\n' -r rm --)")|awk '!x[$0]++'|crontab -
+(crontab -l 2>/dev/null; echo "*/2 * * * * (cd ~/GitRepos/pyxis/servers/plate_solver/output && ls -tp | grep -v '/$' | tail -n +21 | xargs -d '\n' -r rm --)")|awk '!x[$0]++'|crontab -
+
+#Start terminals
+screen -S DextraServers -c screen_config_dextra;
