70e566dd0390193e6df595e989df096caa4b94d0
Jonah Hansen <jonah.hansen@anu.edu.au>
2023-10-10 10:22:20 +1100

    Editted Yepkit hub commands

--- flush_FLIR.sh
+++ flush_FLIR.sh
@@ -3,9 +3,17 @@
 trap 'flush_cache $1 $2' EXIT
 
 flush_cache(){
-    cat ~/pyxis/screen_configs/pw | sudo -S 'ykushcmd ykush -s $1 -d $2'
-    sleep 1
-    cat ~/pyxis/screen_configs/pw | sudo -S 'ykushcmd ykush -s $1 -u $2'
+    a=$1
+    b=${a:1:1}
+    if [ $b == "3" ]; then
+        cat ~/pyxis/screen_configs/pw | sudo -S 'ykushcmd ykush3 -s $1 -d $2'
+        sleep 1
+        cat ~/pyxis/screen_configs/pw | sudo -S 'ykushcmd ykush3 -s $1 -u $2'
+    else
+        cat ~/pyxis/screen_configs/pw | sudo -S 'ykushcmd ykush -s $1 -d $2'
+        sleep 1
+        cat ~/pyxis/screen_configs/pw | sudo -S 'ykushcmd ykush -s $1 -u $2'
+    fi
 }
 
 source $3
